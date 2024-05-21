8c0185cf50bbccba39a37aad1f20017f300b61c9
cyou664 <cyou664@wgu.edu>
2024-05-20 21:13:44 -0600

    Creates a product service

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { map } from 'rxjs'; // might need to be /operator
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    )
+  }
 }
+
+  interface GetResponse {
+    _embedded: {
+      products: Product[];
+    }
+  }
+
